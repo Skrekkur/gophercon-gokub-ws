5da1a03df4fc6fc87c24d50d9a297ef7c184a029
Aevar Kvaran <skrekkur@gmail.com>
2018-05-31 21:17:00 +0000

    charts

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -2,49 +2,55 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Skrekkur/gophercon-gokub-ws/pkg/routing"
 	"github.com/Skrekkur/gophercon-gokub-ws/pkg/webserver"
 )
 
+func startServer(port string, router http.Handler, friendlyName string) *webserver.WebServer {
+
+	if len(port) == 0 {
+		log.Fatal("Service port wasn't set for" + friendlyName)
+	}
+
+	log.Printf(friendlyName + " Service is starting on port:" + port)
+	ws := webserver.New("", port, router)
+	go func() {
+		log.Fatal(ws.Start())
+	}()
+	return ws
+}
 func main() {
 	//$env:SERVICE_PORT = 1000
 
 	shutdown := make(chan error, 2)
 	interrupt := make(chan os.Signal, 1)
 
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+
 	port := os.Getenv("SERVICE_PORT")
-	if len(port) == 0 {
-		log.Fatal("Service port wasn't set")
-	}
-	log.Printf("Service is starting on port:" + port)
-	r := routing.BaseRouter()
-
-	ws := webserver.New("", port, r)
-	go func() {
-		log.Fatal(ws.Start())
-	}()
-
 	internalPort := os.Getenv("INTERNAL_PORT")
-	if len(internalPort) == 0 {
-		log.Fatal("Internal port wasn't set")
-	}
+
+	normalRouter := routing.BaseRouter()
 
 	diagnosticsRouter := routing.DiagnosticsRouter()
-	diagnosticsServer := webserver.New(
-		"", internalPort, diagnosticsRouter,
-	)
-	log.Fatal(diagnosticsServer.Start())
+	ws := startServer(port, normalRouter, "normalServer")
+	internalServer := startServer(internalPort, diagnosticsRouter, "healthService")
 
 	select {
 	case killSignal := <-interrupt:
-		log.Printf(" Getting gilled %s", killSignal)
+		log.Printf(" Getting killed %s", killSignal)
 
 	case err := <-shutdown:
 		log.Printf("Got an error shutting down%s", err)
 	}
 
+	log.Printf("Stopping Services")
 	ws.Stop()
-	diagnosticsServer.Stop()
+	internalServer.Stop()
+	os.Exit(0)
 }
